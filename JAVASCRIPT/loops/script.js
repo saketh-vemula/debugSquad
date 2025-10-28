2d292d551a152dc41030013f8bdbe5f0e2431bbf
saketh <abc@gmail.com>
2025-10-28 11:44:22 +0530

    loops

---
+++ JAVASCRIPT/loops/script.js
@@ -0,0 +1,25 @@
+/* For Loop: it is a looping statement used to iterate the block of code multiple times
+    until the Given condition become false.
+    => For loop Use when we know the how many times the loop will iterate.
+    => Syntax:
+        => for(Initialization; Condition; Increment/ decrement){
+            //block of code;
+        }
+
+
+    => How it Works: 
+    =>1. the interpreter starts from initialization.
+    =>2. then check for condition, if condition true it enters into inside block or condition becom false the control exit from block.
+    =>3. If true exicute the block of code then control move to increment/decrement.
+    =>4. then again check the condition with value if true repeate the process, if false exit from the block.
+*/
+
+// for(let i=1; i<=10; i++){
+//     console.log("Hello "+i);
+// }
+
+// let num=20;
+// for(;num<=30;){
+//      num++;
+//     console.log(num);   
+// }
