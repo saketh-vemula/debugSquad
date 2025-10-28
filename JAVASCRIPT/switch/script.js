d3686762776321766de5ff8440fe0c1886d9599c
saketh <abc@gmail.com>
2025-10-28 11:44:36 +0530

    switch

---
+++ JAVASCRIPT/switch/script.js
@@ -0,0 +1,57 @@
+
+    //  Switch case: 
+    //  *switch statement also a control statement used to execute the purticular block code by matching with given expression in switch().
+    //  * Simple it is alternate Form of else if ladder.
+    //  *But in "switch case"  it execute when given value or expression equals to each other it only comparing the values.
+    
+   /* let day=parseInt(prompt("enter the Day Number: "));
+    switch(day){
+        case 1:
+            document.write("It's Monday");
+            break;
+
+        case 2:
+            document.write("It's Tuesday");
+            break;
+
+        case 3:
+            document.write("It's Wednesday");
+            break;  
+            
+        case 4:
+            document.write("It's Thursday");
+            break;   
+            
+        case 5:
+            document.write("It's Friday");
+            break; 
+            
+        case 6:
+            document.write("It's Saturday");
+            break;  
+            
+        case 7:
+            document.write("It's Sunday");
+            break; 
+            
+        default:
+            document.write("ExcuseMe Week has only 7 days, Please Enter within that!!!");    
+    }*/
+
+    // ============================================================================================================
+    
+    // default is optional in switch case:
+    switch(true){
+        // default :
+        //      document.write("Default Executed");
+        //      break;
+
+        case 1:
+            document.write("Case 1 Executed");
+            break;
+
+        case true:
+            document.write("Case 2 Executed");
+            break;
+
+    }
